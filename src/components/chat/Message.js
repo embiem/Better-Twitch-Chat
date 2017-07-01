700547a4e75b3991fa0ba53e74ba1b7595c2534a
MartinBeierling <mbeierling@gmail.com>
2017-07-02 01:45:32 +0200

    Move chat-styles into Chat.css & adds like/dislike buttons to messages
    The buttons prepare for our Machine Learning implementation to filter
    bad messages out.

--- src/components/chat/Message.js
+++ src/components/chat/Message.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import IconButton from 'material-ui/IconButton';
 import Username from './Username';
 
 const Message = props => {
@@ -6,6 +7,16 @@ const Message = props => {
     <div className="Chat-Message">
       <Username channel={props.channel} user={props.user} />
       <div className="Message">{props.message}</div>
+      <div className="Vote">
+        <IconButton
+          iconClassName="fa fa-thumbs-o-up"
+          onTouchTap={() => console.log(`Liked '${props.message}'`)}
+        />
+        <IconButton
+          iconClassName="fa fa-thumbs-o-down"
+          onTouchTap={() => console.log(`Disliked '${props.message}'`)}
+        />
+      </div>
     </div>
   );
 };
